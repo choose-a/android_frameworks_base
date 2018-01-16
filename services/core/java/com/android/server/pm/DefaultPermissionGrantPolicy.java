c142d571e7add720baeb596fdb5bbb5ffe32dbe0
Humberto Borba <humberos@gmail.com>
2018-01-16 13:58:22 -0200

    base: Fix chromium permissions
    Change-Id: Ie67d6939b4ef1eb7afc5fbafab0429a8485fe5c8
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -739,6 +739,14 @@ final class DefaultPermissionGrantPolicy {
                         STORAGE_PERMISSIONS, true, userId);
             }
 
+            // Chromium Permission
+            PackageParser.Package chromiumPackage = getDefaultProviderAuthorityPackageLPr(
+                    "org.chromium.chrome", userId);
+            if (chromiumPackage != null) {
+                grantRuntimePermissionsLPw(chromiumPackage, CONTACTS_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(chromiumPackage, STORAGE_PERMISSIONS, userId);
+            }
+
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
         }
     }
